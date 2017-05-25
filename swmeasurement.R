533fd958dd84e026f1223cd04f1c027decc947f9
kishore <kishore3886@gmail.com>
2017-05-25 14:50:56 -0400

    min, max,mean  calculated

--- swmeasurement.R
+++ swmeasurement.R
@@ -10,6 +10,7 @@ seed<-1234
 seed_square=0
 start=0
 end=0
+sum_Random_Numbers=0
 #rand_num_df<- data.frame(number=numeric())
 rand_num_df<-numeric(100)
 
@@ -39,7 +40,47 @@ rand_generater<- function(){
   
 }
 
-
-    print(rand_generater())
+    #print(rand_generater())
+ random_Numbers<- rand_generater()
+ random_Numbers[3]
+ typeof(random_Numbers)
+ print(length(random_Numbers))
     #print(seed_square)
+    min_Number=2565
+    max_Number=0
+    random_Numbers_median=0
+    typeof(random_Numbers_median)
+    #finding minimum
+
+    for(i in 1:100)
+    {
+      print(i)
+      print(random_Numbers[i])
+      if(random_Numbers[i]<min_Number)
+       {
+       min_Number= random_Numbers[i]
+     }
+   
+    }
     
+#finding maximum
+    
+    for(i in 1:100)
+    {
+      if(random_Numbers[i]>max_Number)
+      {
+        max_Number= random_Numbers[i]
+      }
+      
+    }
+#FINDING MEAN 
+    for(i in 1:100)
+    {
+      sum_Random_Numbers<-sum_Random_Numbers+random_Numbers[i]
+      
+    }
+    
+    random_Numbers_Median <- sum_Random_Numbers/100
+    min_Number
+    max_Number
+    random_Numbers_Median
